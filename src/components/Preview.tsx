7a0ae8d1fbdbdb1d88fd514fb145b2590ff4329f
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-03-14 15:23:18 -0300

    lint corrections

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -4,7 +4,7 @@ import { PostPreview } from "../types/PostPreview";
 export default function Preview (props: PostPreview) {
     return (
         <div>
-            <Link href={`/posts/${props.slug}`}><h1 className="font-title text-3xl cursor-pointer">{props.title}</h1></Link>
+            <Link href={`/posts/${props.slug}`} passHref><h1 className="font-title text-3xl cursor-pointer">{props.title}</h1></Link>
             <p className="text-gray-500">{props.date}</p>
             <p className="">{props.description}</p>
             <div className="py-4"><div className="w-full border-t hr border-gray-300"></div></div>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from 'next/image';
 
 export default function About () {
     return(
@@ -10,7 +11,7 @@ export default function About () {
         
         <main className="flex gap-5 md:gap-10 md:mt-10 mt-5 flex-col md:flex-row">
             <div className="self-center">
-                <img className='md:w-full w-36 rounded-full' src='https://pbs.twimg.com/profile_images/1498682094314311687/-DOihTkC_400x400.jpg'/>
+                <Image alt='profile picture' className='md:w-full w-36 rounded-full' src='https://pbs.twimg.com/profile_images/1498682094314311687/-DOihTkC_400x400.jpg'/>
             </div>
             <div>
                 <h1 className="font-title text-3xl font-bold">Welcome</h1>
