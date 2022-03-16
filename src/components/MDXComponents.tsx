ef3309a6096f74787854938e957e4d015bb631d6
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-03-16 17:26:28 -0300

    fixing

--- src/components/MDXComponents.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react';
-
-// Declaring components here makes the css applied local; i.e only on markdown instead of on the entire site.
-export const components = {
-    h1: ({ children }: { children: ReactNode }) => <h3 className="font-title text-3xl leading-9">{children}</h3>,
-    h2: ({ children }: { children: ReactNode }) => <h3 className="font-title text-2xl leading-9">{children}</h3>,
-    h3: ({ children }: { children: ReactNode }) => <h3 className="font-title text-2xl leading-9">{children}</h3>
-}
